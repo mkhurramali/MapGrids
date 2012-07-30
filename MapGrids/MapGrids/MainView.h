0a64e77eb207b5092ff066764982c097b3b98d5a
Khurram Ali <khurram@Khurrams-MacBook.local>
2012-07-30 15:06:26 +0500

    1. Basic iPhone app with one view

---
+++ MapGrids/MapGrids/MainView.h
@@ -0,0 +1,13 @@
+//
+//  MainView.h
+//  MapGrids
+//
+//  Created by Khurram Ali on 7/30/12.
+//  Copyright (c) 2012 RIKSOF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainView : UIViewController
+
+@end
---
+++ MapGrids/MapGrids/MainView.m
@@ -0,0 +1,44 @@
+//
+//  MainView.m
+//  MapGrids
+//
+//  Created by Khurram Ali on 7/30/12.
+//  Copyright (c) 2012 RIKSOF. All rights reserved.
+//
+
+#import "MainView.h"
+
+@interface MainView ()
+
+@end
+
+@implementation MainView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
